a39606f0056546616ac30bce409ce7d9c260e359
taelyon <taelyon@naver.com>
2025-09-28 15:13:13 +0900

    update .gitignore and japan-travel.ts

--- api/japan-travel.ts
+++ api/japan-travel.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { handleJapanTravelAction } from '../server/japanTravelService';
+import { handleJapanTravelAction } from '../server/japanTravelService.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
